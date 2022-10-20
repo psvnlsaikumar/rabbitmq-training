8e692c717a2ebe28b8cecc4b73a57399efd86bbb
psvnl sai kumar <psvnlsaikumar@gmail.com>
2022-10-21 00:36:41 +0530

    changes for working queue

--- producer/src/main/java/com/training/rabbitmq/producer/Config.java
+++ producer/src/main/java/com/training/rabbitmq/producer/Config.java
@@ -13,10 +13,18 @@ public class Config {
 
     public static final String QUEUE = "user_event_queue";
     public static final String TOPIC_EXCHANGE = "user_event_exchange";
-    public static final String ROUTING_KEY = "user_event_routing_key";
 
-    public static final String QUEUE_A = "queue_a";
-    public static final String QUEUE_B = "queue_b";
+    public static final String ROUTING_KEY = "user_event_routing_key";
+    public static final String ROUTING_KEY_A = "queue.a";
+    public static final String ROUTING_KEY_B = "queue.b";
+    public static final String ROUTING_KEY_C = "queue.*";
+
+    public static final String FANOUT_EXCHANGE = "exchange.fanout";
+    public static final String QUEUE_A = "queue.a";
+    public static final String QUEUE_B = "queue.b";
+
+    public static final String QUEUE_AUTO = "queue.auto";
+
 
     @Bean
     public Queue queue(){
@@ -34,9 +42,17 @@ public class Config {
     }
 
     @Bean
+    public Queue queueAuto(){
+        return new Queue(QUEUE_AUTO);
+    }
+
+    @Bean
     public TopicExchange exchange(){
         return new TopicExchange(TOPIC_EXCHANGE);
     }
+
+    @Bean
+    public FanoutExchange exchangeFan() {return new FanoutExchange(FANOUT_EXCHANGE);}
 
     @Bean
     public Binding binding(Queue queue, TopicExchange exchange){
@@ -45,12 +61,32 @@ public class Config {
 
     @Bean
     public Binding bindingA(Queue queueA, TopicExchange exchange){
-        return BindingBuilder.bind(queueA).to(exchange).with(ROUTING_KEY);
+        return BindingBuilder.bind(queueA).to(exchange).with(ROUTING_KEY_A);
     }
 
     @Bean
     public Binding bindingB(Queue queueB, TopicExchange exchange){
-        return BindingBuilder.bind(queueB).to(exchange).with(ROUTING_KEY);
+        return BindingBuilder.bind(queueB).to(exchange).with(ROUTING_KEY_B);
+    }
+
+    @Bean
+    public Binding bindingAuto(Queue queueAuto, TopicExchange exchange){
+        return BindingBuilder.bind(queueAuto).to(exchange).with(ROUTING_KEY_C);
+    }
+
+    @Bean
+    public Binding bindingD(Queue queue, FanoutExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange);
+    }
+
+    @Bean
+    public Binding bindingE(Queue queueA, FanoutExchange exchange){
+        return BindingBuilder.bind(queueA).to(exchange);
+    }
+
+    @Bean
+    public Binding bindingF(Queue queueB, FanoutExchange exchange){
+        return BindingBuilder.bind(queueB).to(exchange);
     }
 
     @Bean
--- producer/src/main/java/com/training/rabbitmq/producer/Publisher.java
+++ producer/src/main/java/com/training/rabbitmq/producer/Publisher.java
@@ -1,8 +1,11 @@
 package com.training.rabbitmq.producer;
 
 
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +19,40 @@ public class Publisher {
     @Autowired
     private RabbitTemplate template;
 
+    @Autowired
+    private FanoutExchange fanoutExchange;
+
+    @Autowired
+    private TopicExchange topicExchange;
+
     @PostMapping("/publish")
     public String publishMessage(@RequestBody Event message){
         message.setEventId(UUID.randomUUID().toString());
         message.setEventDate(new Date());
         message.setEventType(message.getEventType());
         template.convertAndSend(Config.TOPIC_EXCHANGE, Config.ROUTING_KEY, message);
+
+
+        return "send message";
+    }
+
+    @PostMapping("/fanout")
+    public String fanoutMessage(@RequestBody Event message){
+        message.setEventId(UUID.randomUUID().toString());
+        message.setEventDate(new Date());
+        message.setEventType(message.getEventType());
+        template.convertAndSend(fanoutExchange.getName(),"", message);
+
+
+        return "send message";
+    }
+
+    @PostMapping("/topic/{routingkey}")
+    public String topicMessage(@RequestBody Event message, @PathVariable String routingkey){
+        message.setEventId(UUID.randomUUID().toString());
+        message.setEventDate(new Date());
+        message.setEventType(message.getEventType());
+        template.convertAndSend(topicExchange.getName(),routingkey, message);
 
 
         return "send message";
